768ebdae9b97d5f906bc182cb452370fa4abee14
Amruta Muthal <amruta.muthal.1992@gmail.com>
2021-05-21 01:11:25 +0530

    Updated visulalization layout

--- src/visualization/visualization.py
+++ src/visualization/visualization.py
@@ -11,11 +11,16 @@ import PIL
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
 import matplotlib.patches as patches
+import SessionState
 
 def main():
     # Render the readme as markdown using st.markdown.
+    session_state = SessionState._get_state()
+    if session_state.page_number is None:
+        session_state.page_number = 0
     
-    f = open(os.path.join(path,'visualization','instructions.md'), "r")
+    st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
+    f = open(os.path.join(path,'instructions.md'), "r")
     readme_text = st.markdown(f.read())
     
     # Once we have the dependencies, add a selector for the app mode on the sidebar.
@@ -30,13 +35,12 @@ def main():
         st.code(f.read())
     elif app_mode == "Run the app":
         readme_text.empty()
-        run_the_app()
+        run_the_app(session_state)
         
-def run_the_app():
+def run_the_app(session_state):
     
     labels = os.listdir(anno_source)
     object_type = st.sidebar.selectbox("Search for which objects?", labels, 1)
-    
     image_list = os.listdir(os.path.join(anno_source,object_type,'bbox'))
     image_list = [name.split('.')[0] for name in image_list]
 
@@ -50,16 +54,33 @@ def run_the_app():
     colours = cv2.cvtColor(colours,cv2.COLOR_HSV2RGB)[0]
     label_col = colours[np.where(np.array(labels)==object_type)][0]
     
-    part_disp = st.checkbox("Display parts")
-    bbox_disp = st.checkbox("Display bounding box view")
-    obbox_disp = st.checkbox("Display oriented bounding box view")
+    part_disp = st.sidebar.checkbox("Display parts")
+    bbox_disp = st.sidebar.checkbox("Display bounding box view")
+    obbox_disp = st.sidebar.checkbox("Display oriented bounding box view")
+        
+    prev_button, _ ,next_button = st.beta_columns([1, 10, 1])
+    last_page = len(image_list)-1
+    session_state.page_number = min(session_state.page_number,last_page)
+    if next_button.button("Next"):
+
+        if session_state.page_number + 1 > last_page:
+            session_state.page_number = 0
+        else:
+            session_state.page_number += 1
+
+    if prev_button.button("Previous"):
+
+        if session_state.page_number - 1 < 0:
+            session_state.page_number = last_page
+        else:
+            session_state.page_number -= 1
     
+    selected_image = st.slider('Select image index',0,len(image_list),session_state.page_number)
+    session_state.page_number = selected_image
+        
     display_multi(object_type,image_list,selected_image,label_col,part_disp,bbox_disp,obbox_disp)
     
-#     if part_disp:
-#         display_parts(object_type,image_list,selected_image,label_col)
-#     else:
-#         display_objects(object_type,image_list,selected_image,label_col)
+    session_state.sync()
 
 def display_objects(object_type,image_list,selected_image,label_col,):
     
@@ -71,7 +92,7 @@ def display_objects(object_type,image_list,selected_image,label_col,):
         disp_list = disp_list[:4]
 
     else:
-        disp_list=[selected_image]
+        disp_list=[image_list[selected_image]]
         
     for image in disp_list:
         with open(os.path.join(anno_source,object_type,'bbox',image+'.json')) as fp:
@@ -93,163 +114,125 @@ def display_objects(object_type,image_list,selected_image,label_col,):
 def display_multi(object_type,image_list,selected_image,label_col,part_disp,bbox_disp,obbox_disp):
     
     font                   = cv2.FONT_HERSHEY_PLAIN
-    fontScale              = 1
-    fontColor              = (255,255,255)
-    lineType               = 2
-    
-#     if part_disp & bbox_disp:
-#         n=3
-#     elif part_disp|bbox_disp:
-#         n=2
-#     else:
-#         n=1
-    n = 1+sum([part_disp,bbox_disp,obbox_disp])
-    
-    disp_image = []
-    if selected_image=='Random':
-        disp_list = image_list[:-1]
-        random.shuffle(disp_list)
-        disp_list = disp_list[:4]
-    else:
-        disp_list=[selected_image]
-    
-    for image in disp_list:
-        with open(os.path.join(anno_source,object_type,'bbox',image+'.json')) as fp:
-            annotation_dict = json.load(fp)
-        raw_image = cv2.imread(os.path.join(img_source,image+'.jpg'))
-        raw_image = cv2.cvtColor(raw_image, cv2.COLOR_BGR2RGB)
-        fig, axes = plt.subplots(1,n, figsize = (12,4))
-        ob_image = raw_image.copy()
-        part_image = raw_image.copy()
-        obb_image = raw_image.copy()
-        t=0
-        im_x_min,im_y_min,im_x_max,im_y_max = raw_image.shape[0],raw_image.shape[1],0,0
-        for idx in annotation_dict.keys():
-            ob_x_min,ob_y_min,ob_x_max,ob_y_max =annotation_dict[idx]['bbox']
-            im_x_min = min(im_x_min,ob_x_min)
-            im_y_min = min(im_y_min,ob_y_min)
-            im_x_max = max(im_x_max,ob_x_max)
-            im_y_max = max(im_y_max,ob_y_max)
-            
-        for idx in annotation_dict.keys():
-            contours, _ = cv2.findContours(np.uint8(np.matrix(annotation_dict[idx]['mask'])),
-                                       cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-
-            ob_image = cv2.drawContours(ob_image, contours, -1,label_col.tolist() , 2)
-            if n>1:
-                axes[0].imshow(ob_image)
-                plt.axis('off')  
-            else:
-                axes.imshow(ob_image)
-                plt.axis('off') 
-            
-#             ob_x_min,ob_y_min,ob_x_max,ob_y_max =annotation_dict[idx]['bbox']
-#             im_x_min = min(im_x_min,ob_x_min)
-#             im_y_min = min(im_y_min,ob_y_min)
-#             im_x_max = max(im_x_max,ob_x_max)
-#             im_y_max = max(im_y_max,ob_y_max)
-            part_dict = annotation_dict[idx]['parts']
-            if part_dict and part_disp:
-                for part in part_dict.keys():
-                    contours, _ = cv2.findContours(np.uint8(np.matrix(part_dict[part]['mask'])),
-                               cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-
-                    part_image = cv2.drawContours(part_image, contours, -1,label_col.tolist() , 1)
-                    loc = np.mean(contours[0],axis=0).astype(int)[0].tolist()
-                    part_image = cv2.putText(part_image,part[0],(loc[0],loc[1]),font,fontScale,fontColor,lineType)
-            
-            if part_dict and obbox_disp:
-                for part in part_dict.keys():
-                    contours,_ = cv2.findContours(np.uint8(np.matrix(part_dict[part]['mask'])),
-                               cv2.RETR_EXTERNAL , cv2.CHAIN_APPROX_NONE)
-                    
-                    for c in contours:
-                        rect = cv2.minAreaRect(c)
-                        box = cv2.boxPoints(rect)
-                        box = np.int0(box)
-                        obb_image = cv2.drawContours(obb_image, [box],0,(0,0,255),1)
-                        
-            if part_dict and bbox_disp:
-                bb_image = raw_image.copy()
-                if part_disp:
-                    axes[2].imshow(raw_image[im_x_min:im_x_max+1,im_y_min:im_y_max+1])
-                    plt.axis('off')  
-                    t = 2
-                else:
-                    axes[1].imshow(raw_image[im_x_min:im_x_max+1,im_y_min:im_y_max+1])
-                    plt.axis('off')  
-                    t = 1
-                for part in part_dict.keys():
-                    
-                    x_min,y_min,x_max,y_max =part_dict[part]['bbox']
-                    rect = patches.Rectangle((y_min-im_y_min,x_min-im_x_min),y_max-y_min,x_max-x_min,
-                                             linewidth=1,edgecolor='r',facecolor='none')
-            
-                    axes[t].add_patch(rect)
-            if part_disp:
-                axes[1].imshow(part_image[im_x_min:im_x_max+1,im_y_min:im_y_max+1])
-            if obbox_disp:
-                axes[t+1].imshow(obb_image[im_x_min:im_x_max+1,im_y_min:im_y_max+1])
-            
-        st.pyplot(fig)
-        #disp_image.append(temp_image)
-     
-    st.subheader('Ground Truth')
-    
-    
-    #for idx,img in enumerate(disp_image):
-    #    st.image(img,caption=disp_list[idx])
-
-def display_parts(object_type,image_list,selected_image,label_col):
-    
-    font                   = cv2.FONT_HERSHEY_PLAIN
-    fontScale              = 1
+    fontScale              = 0.8
     fontColor              = (255,255,255)
     lineType               = 1
-
-    disp_image = []
-    if selected_image=='Random':
-        disp_list = image_list[:-1]
-        random.shuffle(disp_list)
-        disp_list = disp_list[:4]
-    else:
-        disp_list=[selected_image]
     
-    for image in disp_list:
-        with open(os.path.join(anno_source,object_type,'bbox',image+'.json')) as fp:
-            annotation_dict = json.load(fp)
-        temp_image = cv2.imread(os.path.join(img_source,image+'.jpg'))
-        temp_image = cv2.cvtColor(temp_image, cv2.COLOR_BGR2RGB)
-        
-        for idx in annotation_dict.keys():
-            contours, _ = cv2.findContours(np.uint8(np.matrix(annotation_dict[idx]['mask'])),
-                                       cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    colors = [(1, 0, 0),
+          (0.737, 0.561, 0.561),
+          (0.255, 0.412, 0.882),
+          (0.545, 0.271, 0.0745),
+          (0.98, 0.502, 0.447),
+          (0.98, 0.643, 0.376),
+          (0.18, 0.545, 0.341),
+          (0.502, 0, 0.502),
+          (0.627, 0.322, 0.176),
+          (0.753, 0.753, 0.753),
+          (0.529, 0.808, 0.922),
+          (0.416, 0.353, 0.804),
+          (0.439, 0.502, 0.565),
+          (0.784, 0.302, 0.565),
+          (0.867, 0.627, 0.867),
+          (0, 1, 0.498),
+          (0.275, 0.51, 0.706),
+          (0.824, 0.706, 0.549),
+          (0, 0.502, 0.502),
+          (0.847, 0.749, 0.847),
+          (1, 0.388, 0.278),
+          (0.251, 0.878, 0.816),
+          (0.933, 0.51, 0.933),
+          (0.961, 0.871, 0.702)]
+    colors = (np.asarray(colors)*255)
 
-            temp_image = cv2.drawContours(temp_image, contours, -1,label_col.tolist() , 1)
+    n = sum([part_disp,bbox_disp,obbox_disp])
+    
+    disp_image = []
+    disp_list=[image_list[selected_image]]
+    st.write(str(disp_list[0]))
+    f = open('part_label.json',)
+    part_labels = json.load(f)
+    part_labels = part_labels[object_type]
+    
+    image = disp_list[0]
+    with open(os.path.join(anno_source,object_type,'bbox',image+'.json')) as fp:
+        annotation_dict = json.load(fp)
+    raw_image = cv2.imread(os.path.join(img_source,image+'.jpg'))
+    raw_image = cv2.cvtColor(raw_image, cv2.COLOR_BGR2RGB)
+    fig, axes = plt.subplots(1,n, figsize = (12,4))
+    ob_image = raw_image.copy()
+    part_image = raw_image.copy()
+    obb_image = raw_image.copy()
+    t=0
+    im_x_min,im_y_min,im_x_max,im_y_max = raw_image.shape[0],raw_image.shape[1],0,0
+    num = len(annotation_dict.keys())
+    object_idx = st.radio('Select the object you want to focus', list(range(num)),0)
+    idx = list(annotation_dict.keys())[object_idx]
+    im_x_min,im_y_min,im_x_max,im_y_max =annotation_dict[idx]['bbox']
+
+    t=-1
+
+    part_dict = annotation_dict[idx]['parts']
+    if part_dict and part_disp:
+        t+=1
+        axes[t].imshow(part_image[im_x_min:im_x_max+1,im_y_min:im_y_max+1], interpolation='none')
+        overlay = np.zeros(part_image[im_x_min:im_x_max+1,im_y_min:im_y_max+1].shape)
+        for part in part_dict.keys():
+            mask = np.repeat(np.expand_dims(np.matrix(part_dict[part]['mask']),axis=-1),3,axis=2)
+            mask = np.uint(mask*np.expand_dims(np.matrix(colors[part_labels[part]-1]),axis=0))
+            overlay+= mask[im_x_min:im_x_max+1,im_y_min:im_y_max+1]
+            
+              
+        axes[t].imshow(overlay,interpolation='none',alpha=0.2)
+
         
-            part_dict = annotation_dict[idx]['parts']
-            if part_dict:
-                for part in part_dict.keys():
-                    contours, _ = cv2.findContours(np.uint8(np.matrix(part_dict[part]['mask'])),
+
+    if part_dict and obbox_disp:
+        t+=1
+        for part in part_dict.keys():
+            contours,_ = cv2.findContours(np.uint8(np.matrix(part_dict[part]['mask'])),
+                       cv2.RETR_EXTERNAL , cv2.CHAIN_APPROX_NONE)
+
+            for c in contours:
+                rect = cv2.minAreaRect(c)
+                box = cv2.boxPoints(rect)
+                box = np.int0(box)
+                obb_image = cv2.drawContours(obb_image, [box],0,colors[part_labels[part]-1],1)
+        axes[t].imshow(obb_image[im_x_min:im_x_max+1,im_y_min:im_y_max+1])
+
+    if part_dict and bbox_disp:
+        bb_image = raw_image.copy()
+        t +=1
+        axes[t].imshow(raw_image[im_x_min:im_x_max+1,im_y_min:im_y_max+1])
+        plt.axis('off')  
+
+        for part in part_dict.keys():
+
+            x_min,y_min,x_max,y_max =part_dict[part]['bbox']
+            rect = patches.Rectangle((y_min-im_y_min,x_min-im_x_min),y_max-y_min,x_max-x_min,
+                                     linewidth=1,edgecolor=colors[part_labels[part]-1]/255,facecolor='none')
+
+            axes[t].add_patch(rect)
+    
+    st.pyplot(fig)
+    
+    fig2, axes2 = plt.subplots(1,1)
+    contours, _ = cv2.findContours(np.uint8(np.matrix(annotation_dict[idx]['mask'])),
                                cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 
-                    temp_image = cv2.drawContours(temp_image, contours, -1,label_col.tolist() , 1)
-                    loc = np.mean(contours[0],axis=0).astype(int)[0].tolist()
-                    temp_image = cv2.putText(temp_image,part[0][0],(loc[0],loc[1]),font,fontScale,fontColor,lineType)
-                        
-        disp_image.append(temp_image)
+    ob_image = cv2.drawContours(ob_image, contours, -1,label_col.tolist() , 2)
+    
+    axes2.imshow(ob_image)
+    axes2.axis('off')  
+     
+    st.pyplot(fig2)
      
     st.subheader('Ground Truth')
     
-    
-    for idx,img in enumerate(disp_image):
-        st.image(img,caption=disp_list[idx])
-        
 
 if __name__ == "__main__":
     
     curr_path = os.getcwd()
     path = Path(curr_path)
-    anno_source = os.path.join(path,'PASCAL-VOC','xybb-objects')
-    img_source = os.path.join(path,'PASCAL-VOC','scene')
+    anno_source = os.path.join(path.parent,'PASCAL-VOC','xybb-objects')
+    img_source = os.path.join(path.parent,'PASCAL-VOC','scene')
     main()
