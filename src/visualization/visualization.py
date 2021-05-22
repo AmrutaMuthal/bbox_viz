7138947f8ac791823b2852dc5c6ab9477e278051
Amruta Muthal <amruta.muthal.1992@gmail.com>
2021-05-22 13:58:33 +0530

    Bug fix for mask color

--- src/visualization/visualization.py
+++ src/visualization/visualization.py
@@ -150,18 +150,20 @@ def display_multi(object_type,image_list,selected_image,label_col,part_disp,bbox
             else:
                 axes.imshow(part_image[im_x_min:im_x_max+1,im_y_min:im_y_max+1], interpolation='none')
             
-            overlay = np.zeros(part_image[im_x_min:im_x_max+1,im_y_min:im_y_max+1].shape)
+            overlay = np.zeros(part_image[im_x_min:im_x_max+1,im_y_min:im_y_max+1].shape,'int')
             for part in part_dict.keys():
                 mask = np.repeat(np.expand_dims(np.matrix(part_dict[part]['mask']),axis=-1),3,axis=2)
-                mask = np.uint(mask*np.expand_dims(np.matrix(colors[part_labels[part]-1]),axis=0))
+                mask = np.uint(mask*np.expand_dims(np.matrix(colors[part_labels[part]-1]).astype('int'),axis=0))
                 overlay+= mask[im_x_min:im_x_max+1,im_y_min:im_y_max+1]
+                
 
             if n>1:
                 axes[t].imshow(overlay,interpolation='none',alpha=0.2)
+                axes[t].axis('off')  
+                
             else:
                 axes.imshow(overlay,interpolation='none',alpha=0.2)
-                
-    
+                axes.axis('off')  
 
 
         if part_dict and obbox_disp:
@@ -174,21 +176,23 @@ def display_multi(object_type,image_list,selected_image,label_col,part_disp,bbox
                     rect = cv2.minAreaRect(c)
                     box = cv2.boxPoints(rect)
                     box = np.int0(box)
-                    obb_image = cv2.drawContours(obb_image, [box],0,colors[part_labels[part]-1],1)
+                    obb_image = cv2.drawContours(obb_image, [box],0,colors[part_labels[part]-1])
             if n>1:
                 axes[t].imshow(obb_image[im_x_min:im_x_max+1,im_y_min:im_y_max+1])
+                axes[t].axis('off')  
             else:
                 axes.imshow(obb_image[im_x_min:im_x_max+1,im_y_min:im_y_max+1])
-                
+                axes.axis('off')  
+            
         if part_dict and bbox_disp:
             bb_image = raw_image.copy()
             t +=1
             if n > 1:
                 axes[t].imshow(raw_image[im_x_min:im_x_max+1,im_y_min:im_y_max+1])
+                axes[t].axis('off')
             else:
                 axes.imshow(raw_image[im_x_min:im_x_max+1,im_y_min:im_y_max+1])
-                
-            plt.axis('off')  
+                axes.axis('off')  
 
             for part in part_dict.keys():
 
